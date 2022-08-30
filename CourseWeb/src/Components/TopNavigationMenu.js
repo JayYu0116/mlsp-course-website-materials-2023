66c94886af8240855dc5fc1e57cdfc18ab8ab629
zhaizhongyuan <zhaizhongyuanzhuanxu@gmail.com>
2022-08-30 14:59:42 -0400

    challenge tab name update

--- CourseWeb/src/Components/TopNavigationMenu.js
+++ CourseWeb/src/Components/TopNavigationMenu.js
@@ -8,7 +8,7 @@ const navigation = [
 { name: 'Home', href: '/' },
 { name: 'Lectures', href: '/lectures' },
 { name: 'Homework', href: '/homework' },
-{ name: 'Project', href: '/project' },
+{ name: 'Challenge', href: '/project' },
 ]
 
 export default function TopNavigationMenu() {
