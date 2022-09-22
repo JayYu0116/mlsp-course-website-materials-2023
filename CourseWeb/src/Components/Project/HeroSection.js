3d3125565c8d853f2f6824c13d6d11881c6da993
zhaizhongyuan <zhaizhongyuanzhuanxu@gmail.com>
2022-09-21 23:15:22 -0400

    challenge text update

--- CourseWeb/src/Components/Project/HeroSection.js
+++ CourseWeb/src/Components/Project/HeroSection.js
@@ -4,11 +4,11 @@ export default function HeroSection() {
 			<div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
 				<div className="text-center">
 					<p className="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-6xl">
-						Course Challenge
+						Class Challenge
 					</p>
 					<p className="max-w-4xl mt-5 mx-auto text-xl text-gray-500">
 						{/* We'll keep this section updated as projects group form and possible topics are announced. We have always had impressive projects, so we're really excited to see what you do! Projects in this course have gone on to become Ph.D. theses and award-winning papers. Starting thinking of topics you want to work on right away! */}
-						We're reconfiguring the project component of this course into a rather more intriguing challenge component, stay tuned!
+						Accept the challenge!
 					</p>
 				</div>
 			</div>
--- CourseWeb/src/Components/Project/ProjectIdeasSection.js
+++ CourseWeb/src/Components/Project/ProjectIdeasSection.js
@@ -4,14 +4,14 @@ export default function ProjectIdeasSection() {
 			<h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl my-2">
 				Challenge Ideas
 			</h2>
-			{/* <p className="text-xl leading-7 text-gray-500 mb-4">
-				Below is a link to several project ideas compiled by the course staff. The ideas enumerated in the document are intentionally brief: we want you to be creative with it. If you have any questions about the ideas, feel free to reach out over Piazza. You do not have to use any of these ideas for your project, but they can act as a nice starting point as you and your team map out what you want to work on.
+			<p className="text-xl leading-7 text-gray-500 mb-4">
+				Below is the document elaborating on each of the chanllenges that Professor Ramakrishnan briefly went over in class. Have fun exploring!
 			</p>
 			<p className="text-xl leading-7 text-gray-500 mb-4">
 				<a href="https://piazza.com/class/l3n59b0dy7l4q6/post/6" className="text-indigo-600">
 					Download the list of project ideas here.
 				</a>
-			</p> */}
+			</p>
 		</div>
 	)
 }
