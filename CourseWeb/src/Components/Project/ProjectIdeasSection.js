a033b8e2988d5fba4ca3c198ef19638c8059133d
zhaizhongyuan <zhaizhongyuanzhuanxu@gmail.com>
2022-09-21 23:24:41 -0400

    challenge file update

--- CourseWeb/src/Components/Project/ProjectIdeasSection.js
+++ CourseWeb/src/Components/Project/ProjectIdeasSection.js
@@ -8,8 +8,8 @@ export default function ProjectIdeasSection() {
 				Below is the document elaborating on each of the chanllenges that Professor Ramakrishnan briefly went over in class. Have fun exploring!
 			</p>
 			<p className="text-xl leading-7 text-gray-500 mb-4">
-				<a href="https://piazza.com/class/l3n59b0dy7l4q6/post/6" className="text-indigo-600">
-					Download the list of project ideas here.
+				<a href='/project/MLSP_F22_Challenge.pdf' className="text-indigo-600">
+					Download the List of Class Chanllenges
 				</a>
 			</p>
 		</div>
